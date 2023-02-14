4d0c4426856d81b0e84aa6fe9a28dc6c69816048
Dennis Psaroudakis <psaroudakis@ipk-gatersleben.de>
2023-02-14 22:26:06 +0100

    clean up exported sequences a bit

--- 1_core_response/1.5_export_core_sequences.sh
+++ 1_core_response/1.5_export_core_sequences.sh
@@ -6,12 +6,13 @@ for stress_type in `tail -n+2 "output/1.4_core_response_genes.csv" | cut -f1 -d"
   for direction in `tail -n+2 "output/1.4_core_response_genes.csv" | cut -f2 -d"," | sort | uniq`; do
     echo $stress_type,$direction
     grep $stress_type,$direction "output/1.4_core_response_genes.csv" | cut -f3 -d"," | sort > id.lst
-    seqtk subseq ../data/ITAG4.1_proteins.lfs.fasta.gz id.lst > output/sequences/${stress_type}_${direction}.lfs.fasta
+    seqtk subseq ../data/ITAG4.1_proteins.lfs.fasta.gz id.lst | awk '{print $1}' | sed 's/\*//g' > output/sequences/${stress_type}_${direction}.lfs.fasta
   done
 done
 
 # Get a random 500 sequences for baseline
-zgrep ">" ../data/ITAG4.1_proteins.lfs.fasta.gz | cut -f1 -d" " | cut -c2- | shuf | head -500 | sort > id.lst
-seqtk subseq ../data/ITAG4.1_proteins.lfs.fasta.gz id.lst > output/sequences/random_500.lfs.fasta
+#zgrep ">" ../data/ITAG4.1_proteins.lfs.fasta.gz | cut -f1 -d" " | cut -c2- | shuf | head -500 | sort > random.lst
+seqtk subseq ../data/ITAG4.1_proteins.lfs.fasta.gz id.lst | awk '{print $1}' | sed 's/\*//g' > output/sequences/random_500.lfs.fasta
 
-rm id.lst
+rm id.lst random.lst
+
