7395b200c6d08fbfe0741076d3aa2c06a7c44e7d
Dennis Psaroudakis <psaroudakis@ipk-gatersleben.de>
2023-02-14 22:49:01 +0100

    restore 500 random sequences, add interpro families

--- 1_core_response/1.5_export_core_sequences.sh
+++ 1_core_response/1.5_export_core_sequences.sh
@@ -11,8 +11,8 @@ for stress_type in `tail -n+2 "output/1.4_core_response_genes.csv" | cut -f1 -d"
 done
 
 # Get a random 500 sequences for baseline
-#zgrep ">" ../data/ITAG4.1_proteins.lfs.fasta.gz | cut -f1 -d" " | cut -c2- | shuf | head -500 | sort > random.lst
-seqtk subseq ../data/ITAG4.1_proteins.lfs.fasta.gz id.lst | awk '{print $1}' | sed 's/\*//g' > output/sequences/random_500.lfs.fasta
+zgrep ">" ../data/ITAG4.1_proteins.lfs.fasta.gz | cut -f1 -d" " | cut -c2- | shuf | head -500 | sort > random.lst
+seqtk subseq ../data/ITAG4.1_proteins.lfs.fasta.gz random.lst | awk '{print $1}' | sed 's/\*//g' > output/sequences/random_500.lfs.fasta
 
 rm id.lst random.lst
 
