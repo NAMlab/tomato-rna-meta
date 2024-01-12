3dc1bf1ec69b7589ce493bb0d7674ae9f921d294
Dennis Psaroudakis <psaroudakis@ipk-gatersleben.de>
2024-01-12 16:22:49 +0100

    Figure 4A: add pi score calculations, not just p-value

--- 2_curve/curve.R
+++ 2_curve/curve.R
@@ -100,31 +100,87 @@ plt.p$n.genes.lower = log(plt.p$n.genes.lower, base=10)
 plt.p$n.genes.mean = log(plt.p$n.genes.mean, base=10)
 plt.p$n.genes.upper = log(plt.p$n.genes.upper, base=10)
 
+# Now again but using the score of https://doi.org/10.1093/bioinformatics/btr671 
+plt.s = data.frame(s=numeric(), n.genes.upper=numeric(), n.genes.mean=numeric(), n.genes.lower=numeric(), 
+                   tp.upper=numeric(), tp.mean=numeric(), tp.lower=numeric(), 
+                   fp.upper=numeric(), fp.mean=numeric(), fp.lower=numeric(),
+                   stress=character())
+for(stress in c("heat", "drought", "salt")) {
+  samples.stress = samples.annotation[samples.annotation$stress_type == stress,]
+  
+  contrasts = unique(samples.stress$sample.group)
+  
+  res = data.frame(s = numeric(), n.genes=numeric(), true.pos=numeric(), false.pos=numeric())
+  for(s in c(0, 1, 2, 3, 5, 10, 25, 100, 200, 300, 400, 500)) {
+    print(s)
+    for(co in contrasts) {
+      found.genes = str_split_fixed(
+        # They abs() their logFC but since we only care about upregulated genes we can skip that and then
+        # any genes where the overall π score is negative are filtered out as well.
+        diffexp[-log(diffexp[paste0(co, "_FDR")], 10) * diffexp[paste0(co, "_logFC")] > s,]$gene,"\\.", 2)[,1]
+      true.pos = sum(found.genes %in% annotated.genes[annotated.genes$stress == stress & annotated.genes$reasonable_term == "true",]$gene)
+      false.pos = sum(found.genes %in% annotated.genes[annotated.genes$stress == stress & annotated.genes$reasonable_term == "false",]$gene)
+      res = rbind(res, list(s=s, n.genes=length(found.genes), true.pos=true.pos, false.pos=false.pos))
+    }
+  }
+  
+  # Number of genes in intersect set
+  ci.genes = group.CI(n.genes ~ s, res)
+  
+  # Number of Genes annotated with a specific term
+  res$tp = (res$true.pos / (res$n.genes)) * 100 # [%]
+  ci.tp = group.CI(tp ~ s, res)
+  
+  res$fp = (res$false.pos / (res$n.genes)) * 100 # [%]
+  ci.fp = group.CI(fp ~ s, res)
+  
+  
+  m = merge(ci.genes, merge(ci.tp, ci.fp))
+  m$stress = stress
+  plt.s = rbind(plt.s, m)
+}
+
+plt.s$n.genes.lower = log(plt.s$n.genes.lower, base=10)
+plt.s$n.genes.mean = log(plt.s$n.genes.mean, base=10)
+plt.s$n.genes.upper = log(plt.s$n.genes.upper, base=10)
+
 ##### Plots #####
 source("../config.R")
 stress.cols = colors[["stress"]]
 
 pdf("output/curves.pdf", 6.5, 5)
-par(mfrow=c(2,2), family="serif", cex=0.6)
+par(mfrow=c(2,3), family="serif", cex=0.6)
 
 # N Genes Plots
-par(mar=c(2,3,0,0.5))
-plot(plt.p$n.genes.mean ~ plt.p$p.val, type="n", axes=F, ylim=c(0, 4))
-axis(side = 1, las=1, col=F, col.axis="gray52", at = seq(0,100,20), labels=paste0("1e-", seq(0,100,20)))
+par(mar=c(2,4,0,0))
+plot(plt.p$n.genes.mean ~ plt.p$p.val, type="n", axes=F, ylim=c(0, 4), ylab="")
+axis(side = 1, las=1, col=F, col.axis="gray52", at = seq(0,100,25), labels=paste0("1e-", seq(0,100,25)))
 axis(side = 2, las=1, at = 0:4, labels=c(1,10,100,"1k","10k"), col=F, col.axis="gray52")
-text(max(plt.p$p.val), 3.6, "number of selected genes \nby p-value cutoff", pos=2, cex=1.1)
+#text(max(plt.p$p.val), 3.6, "number of selected genes \nby p-value cutoff", pos=2, cex=1.1)
 for(stress in c("heat", "drought", "salt")) {
   d = plt.p[plt.p$stress == stress,]
   polygon(c(d$p.val,rev(d$p.val)),c(d$n.genes.upper,rev(d$n.genes.lower)), col = rgb(t(col2rgb(stress.cols[[stress]])/255), alpha = 0.1), border = FALSE)
   lines(d$n.genes.mean ~ d$p.val, type="l", col=stress.cols[[stress]])
   mtext("|", side=2, at=min(d$n.genes.mean), col=stress.cols[[stress]], cex=0.5)
 }
+mtext("selected genes", side=2, line=2.6, col="gray52", cex=0.6)
 
-par(mar=c(2,1.5,0,0.2))
-plot(plt$n.genes.mean ~ plt$n.contrasts, type="n", ylim=c(0, 4), axes=F)
+par(mar=c(2,2.5,0,0))
+plot(plt.s$n.genes.mean ~ plt.s$s, type="n", axes=F, ylim=c(0, 4), ylab="")
+axis(side = 1, las=1, col=F, col.axis="gray52", at = seq(0,500,100), labels=seq(0,500,100))
+axis(side = 2, las=1, at = 0:4, labels=c(1,10,100,"1k","10k"), col=F, col.axis="gray52")
+#text(max(plt.p$p.val), 3.6, "number of selected genes \nby π score cutoff", pos=2, cex=1.1)
+for(stress in c("heat", "drought", "salt")) {
+  d = plt.s[plt.s$stress == stress,]
+  polygon(c(d$s,rev(d$s)),c(d$n.genes.upper,rev(d$n.genes.lower)), col = rgb(t(col2rgb(stress.cols[[stress]])/255), alpha = 0.1), border = FALSE)
+  lines(d$n.genes.mean ~ d$s, type="l", col=stress.cols[[stress]])
+  mtext("|", side=2, at=min(d$n.genes.mean), col=stress.cols[[stress]], cex=0.5)
+}
+
+plot(plt$n.genes.mean ~ plt$n.contrasts, type="n", ylim=c(0, 4), axes=F, ylab="")
 axis(side = 1, las=1, col=F, col.axis="gray52")
 axis(side = 2, las=1, at = 0:4, labels=c(1,10,100,"1k","10k"), col=F, col.axis="gray52")
-text(max(plt$n.contrasts), 3.6, "number of selected genes \nby number of contrasts (50 simulations)", pos=2, cex=1.1)
+#text(max(plt$n.contrasts), 3.6, "number of selected genes \nby number of contrasts (50 simulations)", pos=2, cex=1.1)
 for(stress in c("heat", "drought", "salt")) {
   d = plt[plt$stress == stress,]
   polygon(c(d$n.contrasts,rev(d$n.contrasts)),c(d$n.genes.upper,rev(d$n.genes.lower)), col = rgb(t(col2rgb(stress.cols[[stress]])/255), alpha = 0.1), border = FALSE)
@@ -136,9 +192,9 @@ text(20, log(20, 10), "drought", col=stress.cols[["drought"]])
 text(25.3, log(100, 10), "heat", col=stress.cols[["heat"]])
 
 # % Annotated plots
-par(mar=c(2,3,1,0.5))
-plot(plt.p$tp.mean ~ plt.p$p.val, type="n", ylim=c(0, 50), axes=F)
-axis(side = 1, las=1, col=F, col.axis="gray52", at = seq(0,100,20), labels=paste0("1e-", seq(0,100,20)))
+par(mar=c(3.5,4,1,0))
+plot(plt.p$tp.mean ~ plt.p$p.val, type="n", ylim=c(0, 50), axes=F, xlab="", ylab="")
+axis(side = 1, las=1, col=F, col.axis="gray52", at = seq(0,100,25), labels=paste0("1e-", seq(0,100,25)))
 axis(side = 2, las=1, col=F, col.axis="gray52", at=seq(0,50,10), labels=paste0(seq(0,50,10), "%"))
 #axis(side = 1, las=1, lwd=0, lwd.ticks = 1, at = seq(0,100,20), labels=as.expression(sapply(seq(0,100,20), FUN=function(x) {bquote(1 ^- .(x))} )))
 for(stress in c("heat", "drought", "salt")) {
@@ -152,15 +208,37 @@ for(stress in c("heat", "drought", "salt")) {
   polygon(c(d$p.val,rev(d$p.val)),c(d$fp.upper,rev(d$fp.lower)), col = rgb(t(col2rgb(stress.cols[[stress]])/255), alpha = 0.1), border = FALSE)
   lines(d$fp.mean ~ d$p.val, type="l", col=stress.cols[[stress]], lty=2)
 }
-text(0.5, 46, "proportion of correctly/incorrectly annotated genes \nby p-value cutoff", pos=4, cex=1.1)
-segments(39, 47, 55, lty=2)
+#text(0.5, 46, "proportion of correctly/incorrectly annotated genes \nby p-value cutoff", pos=4, cex=1.1)
+#segments(39, 47, 55, lty=2)
+mtext("p-value cutoff", side=1, line=2, col="gray52", cex=0.6)
+mtext("(in-)correctly annotated genes", side=2, line=2.6, col="gray52", cex=0.6)
 
+par(mar=c(3.5,2.5,1,0))
+plot(plt.s$tp.mean ~ plt.s$s, type="n", ylim=c(0, 50), axes=F, xlab="", ylab="")
+axis(side = 1, las=1, col=F, col.axis="gray52", at = seq(0,500,100), labels=seq(0,500,100))
+axis(side = 2, las=1, col=F, col.axis="gray52", at=seq(0,50,10), labels=paste0(seq(0,50,10), "%"))
+#axis(side = 1, las=1, lwd=0, lwd.ticks = 1, at = seq(0,100,20), labels=as.expression(sapply(seq(0,100,20), FUN=function(x) {bquote(1 ^- .(x))} )))
+for(stress in c("heat", "drought", "salt")) {
+  d = plt.s[plt.s$stress == stress,]
+  # True positives
+  polygon(c(d$s,rev(d$s)),c(d$tp.upper,rev(d$tp.lower)), col = rgb(t(col2rgb(stress.cols[[stress]])/255), alpha = 0.1), border = FALSE)
+  lines(d$tp.mean ~ d$s, type="l", col=stress.cols[[stress]])
+  mtext("|", side=2, at=max(d$tp.mean), col=stress.cols[[stress]], cex=0.5)
+  
+  # False positives
+  polygon(c(d$s,rev(d$s)),c(d$fp.upper,rev(d$fp.lower)), col = rgb(t(col2rgb(stress.cols[[stress]])/255), alpha = 0.1), border = FALSE)
+  lines(d$fp.mean ~ d$s, type="l", col=stress.cols[[stress]], lty=2)
+}
+#text(0.5, 46, "proportion of correctly/incorrectly annotated genes \nby p-value cutoff", pos=4, cex=1.1)
+#segments(39, 47, 55, lty=2)
+mtext(expression(pi * "-score cutoff"), side=1, line=2, col="gray52", cex=0.6)
 
-plot(plt$tp.mean ~ plt$n.contrasts, type="n", ylim=c(0, 50), axes=F)
+plot(plt$tp.mean ~ plt$n.contrasts, type="n", ylim=c(0, 50), axes=F, xlab="", ylab="")
 axis(side = 1, las=1, col=F, col.axis="gray52")
 axis(side = 2, las=1, col=F, col.axis="gray52", at=seq(0,50,10), labels=paste0(seq(0,50,10), "%"))
-text(0.5, 46, "proportion of correctly/incorrectly annotated genes \nby number of contrasts (50 simulations)", pos=4, cex=1.1)
-segments(11.8, 47, 16.8, lty=2)
+#text(0.5, 46, "proportion of correctly/incorrectly annotated genes \nby number of contrasts (50 simulations)", pos=4, cex=1.1)
+mtext("number of contrasts (50 simulations)", side=1, line=2, col="gray52", cex=0.6)
+#segments(11.8, 47, 16.8, lty=2)
 for(stress in c("heat", "drought", "salt")) {
   d = plt[plt$stress == stress,]
   # True positives
--- config.R
+++ config.R
@@ -1,4 +1,27 @@
 colors = list(
   stress = c("heat"="#D55E00", "drought"="#0072B2", "salt"="#009E73"),
-  tissue = c("anther" = "#CC79A7", "fruit" = "#D55E00", "leaf" = "#009E73", "pollen" = "#F0E442", "seed" = "#E69F00", "seedling" = "#56B4E9", "ovaries"="pink", "root"="brown")
+  tissue = c("anther" = "#CC79A7", "fruit" = "#D55E00", "leaf" = "#009E73", "pollen" = "#F0E442", "seed" = "#E69F00", "seedling" = "#56B4E9", "ovaries"="pink", "root"="brown"),
+  genotype = c(
+    "M82" = "#0000FF",
+    "Moneymaker" = "#FFA500",
+    "Red Setter" = "#008000",
+    "LA1698" = "#FF0000",
+    "Ailsa Craig" = "#800080",
+    "LA1509" = "#A52A2A",
+    "Micro tom" = "#FFC0CB",
+    "Boludo" = "#00FFFF",
+    "LA4024" = "#FFFF00",
+    "Jinlingmeiyu" = "#008080",
+    "IL9-1" = "#800000",
+    "Criollo" = "#808000",
+    "Ferum" = "#000080",
+    "PlovdivXIVa" = "#FF7F50",
+    "LA1420" = "#E6E6FA",
+    "LA0147" = "#FA8072",
+    "Stupicke Polni Rane" = "#4B0082",
+    "Levovil" = "#40E0D0",
+    "Cervil" = "#D2B48C",
+    "Kashi Amrit" = "#708090",
+    "ACE 55" = "#98FF98"
+  )
 )
