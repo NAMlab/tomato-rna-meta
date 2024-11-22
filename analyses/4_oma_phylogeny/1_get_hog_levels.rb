f35792db3c09621e6e516acc9a3f28ef0ccf4016
Dennis Psaroudakis <psaroudakis@ipk-gatersleben.de>
2024-11-22 19:54:27 +0100

    Improve 1_get_hog_levels.rb in response to #1

--- analyses/4_oma_phylogeny/1_get_hog_levels.rb
+++ analyses/4_oma_phylogeny/1_get_hog_levels.rb
@@ -6,26 +6,33 @@ require "json"
 
 puts "target,hogs_with_deepest_levels"
 
-current_name = ""
+current_name = nil
 current_sequence = ""
 
 File.foreach(ARGV.first) do |line|
   if line.start_with?(">")
-    current_name = line[1..-1].chomp
-  else
-    print current_name + ","
-    begin
-      json = JSON.parse(Net::HTTP.get(URI("https://omabrowser.org/api/sequence/?format=json&query=" + line)))
-      hogs = json["targets"].select { |t| t["species"]["taxon_id"] == 4081 }.map { |t| t["oma_hog_id"].split(".").first }.uniq.compact
+    unless current_name.nil?
+      print current_name + ","
+      begin
+        json = JSON.parse(Net::HTTP.get(URI("https://omabrowser.org/api/sequence/?format=json&query=" + current_sequence)))
+        if json.has_key?("targets")
+          hogs = json["targets"].select { |t| t["species"]["taxon_id"] == 4081 }.map { |t| t["oma_hog_id"].split(".").first }.uniq.compact
 
-      hogs.each do |hog|
-        json = JSON.parse(Net::HTTP.get(URI("https://omabrowser.org/api/hog/#{hog}/members/?format=json")))
-        print(hog + "->" + json["level"] + " ")
+          hogs.each do |hog|
+            json = JSON.parse(Net::HTTP.get(URI("https://omabrowser.org/api/hog/#{hog}/members/?format=json")))
+            print(hog + "->" + json["level"] + " ")
+          end
+        else
+          print("ERROR: #{json['detail']}")
+        end
+      rescue Net::ReadTimeout => e
+        print "TIMEOUT"
       end
-    rescue Net::ReadTimeout => e
-      print "TIMEOUT"
+      puts
     end
-    puts
+    current_name = line[1..-1].split.first
+    current_sequence = ""
+  else
+    current_sequence += line.chomp
   end
 end
-
